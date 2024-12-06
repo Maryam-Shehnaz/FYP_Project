034c34a9b9103869b2f1299ea2bfa91c69726430
Maryam <maryamshehnaz006@gmail.com>
2024-12-07 00:48:04 +0500

    welcome screen completed

---
+++ src/screen/HomeScreen.jsx
@@ -0,0 +1,109 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native'
+import React from 'react'
+import { colors } from '../utils/colors'
+import { fonts } from '../utils/fonts'
+import { useNavigation } from '@react-navigation/native'
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    console.log('Navigating to LOGIN');
+    navigation.navigate("LOGIN");
+  };
+  return (
+    <View style={styles.container}>
+      <Image source={require("../assets/logo.jpg")} style={styles.logo}/>
+      <Image source={require("../assets/main.jpg")} style={styles.bannerImage} />
+      <Text style={styles.title}>Lorem ipsum dolor</Text>
+      <Text style={styles.subTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint illum 
+        maxime accusamus totam explicabo quibusdam.
+      </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity 
+        style={[
+        styles.loginButtonWrapper,
+        {backgroundColor: colors.primary},
+
+        ]}
+        onPress={handleLogin}
+        >
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.loginButtonWrapper]}>
+          <Text style={styles.signupButtonText}>Signup</Text>
+        </TouchableOpacity>
+
+
+      </View>
+      
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: "center",
+
+  },
+  logo:{
+    height: 120,
+    width: 140,
+    marginVertical:30,
+  },
+  bannerImage:{
+    marginVertical:20,
+    height:250,
+    width: 231,
+
+  },
+  title:{
+    fontSize:40,
+    fontFamily: fonts.SemiBold,
+    paddingHorizontal: 20,
+    textAlign: "center",
+    color: colors.primary,
+    marginTop: 40,
+
+  },
+  subTitle:{
+    fontSize: 18,
+    textAlign: "center",
+    color: colors.scondary,
+    fontFamily: fonts.Medium,
+    paddingHorizontal: 20,
+    marginVertical: 20,
+
+  },
+  buttonContainer:{
+    
+    marginTop: 20,
+    flexDirection: "row",
+    borderWidth: 2,
+    borderColor: colors.primary,
+    width: "80%",
+    height:60,
+    borderRadius:100,
+  },
+  loginButtonWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+    width:"50%",
+    borderRadius: 98,
+  },
+  loginButtonText:{
+    color: colors.white,
+    fontSize: 18,
+    fontFamily: fonts.SemiBold,
+
+  },
+  signupButtonText:{
+    fontSize: 18,
+    fontFamily: fonts.SemiBold,
+  }
+
+});
