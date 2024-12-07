ba905c297b0623fc84d3cc8284e2fe3bbd3aa927
Maryam <maryamshehnaz006@gmail.com>
2024-12-08 00:49:07 +0500

    change

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -73,7 +73,7 @@ const styles = StyleSheet.create({
   subTitle:{
     fontSize: 18,
     textAlign: "center",
-    color: colors.scondary,
+    color: colors.secondary,
     fontFamily: fonts.Medium,
     paddingHorizontal: 20,
     marginVertical: 20,
