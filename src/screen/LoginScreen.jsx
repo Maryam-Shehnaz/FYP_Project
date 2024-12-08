1b04e762337a50e84fc26232631be0e8e5825a00
Maryam <maryamshehnaz006@gmail.com>
2024-12-08 22:14:38 +0500

    login screen completed

--- src/screen/LoginScreen.jsx
+++ src/screen/LoginScreen.jsx
@@ -1,15 +1,26 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { colors } from '../utils/colors';
 import { fonts } from '../utils/fonts';
 import Ionicons from "react-native-vector-icons/Ionicons";
 import SimpleLineIcons from "react-native-vector-icons/SimpleLineIcons";
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
+  // hook
+  const navigation = useNavigation();
   const [secureEntery, setSecureEntery] = useState(true);
+
+  const handleGoBack = () => {
+    // prop
+    navigation.goBack();
+
+  }
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.backButtonWrapper}>
+      <TouchableOpacity style={styles.backButtonWrapper} onPress=
+      {handleGoBack}>
         <Ionicons name={"arrow-back-outline"} color={colors.primary}
         size={25}
         />
@@ -50,9 +61,19 @@ const LoginScreen = () => {
           <Text style={styles.forgotPasswordText}>Forgot Password?</Text>
 
           </TouchableOpacity>
-          <TouchableOpacity>
-            <Text>Login</Text>
+          <TouchableOpacity style={styles.loginButtonWrapper}>
+            <Text style={styles.loginText}>Login</Text>
           </TouchableOpacity>
+          <Text style={styles.continueText}>or continue with</Text>
+          <TouchableOpacity style={styles.googleButtonContainer}>
+           <Image source={require("../assets/logo.jpg")} style={styles.
+            googleImage}/>
+            <Text style={styles.googleText}>Google</Text>
+          </TouchableOpacity>
+          <View style={styles.footerContainer}>
+            <Text style={styles.accountText}>Don't have an account?</Text>
+            <Text stylel={styles.signupText}>Sign up</Text>
+          </View>
 
         </View>
       
@@ -110,5 +131,63 @@ const styles = StyleSheet.create({
     fontFamily: fonts.SemiBold,
     marginVertical:10,
 
+  },
+  loginButtonWrapper:{
+    backgroundColor:colors.primary,
+    borderRadius:100,
+    marginTop:20,
+
+  },
+  loginText:{
+    color:colors.white,
+    fontSize:20,
+    fontFamily:fonts.SemiBold,
+    textAlign:"center",
+    padding:10,
+  },
+  continueText:{
+    textAlign:"center",
+    marginVertical:20,
+    fontSize:14,
+    fontFamily:fonts.Regular,
+    color:colors.primary,
+  },
+
+  googleButtonContainer:{
+    flexDirection:"row",
+    borderWidth:2,
+    borderColor:colors.primary,
+    borderRadius:100,
+    justifyContent:"center",
+    alignItems:"center",
+    padding:10,
+    gap:10,
+
+  },
+  googleImage:{
+    height:20,
+    width: 20,
+  },
+  googleText:{
+    fontSize:20,
+    fontFamily:fonts.SemiBold,
+  },
+  footerContainer:{
+    flexDirection:"row",
+    justifyContent:"center",
+    alignItems:"center",
+    marginVertical:20,
+    gap:5,
+  },
+  accountText:{
+    color:colors.primary,
+    fontFamily:fonts.Regular,
+  },
+  signupText:{
+    color:colors.primary,
+    fontFamily:fonts.Bold,
+
   }
+
+
 })
